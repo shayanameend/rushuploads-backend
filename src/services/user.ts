870b024175fb718a0b7a8a81fde2d8edef31469a
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-12-05 12:44:34 +0500

    Implement user management functions: create, retrieve, update, and delete users

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,24 +1,20 @@
-import type { Role } from "@prisma/client";
+import type { Prisma, Role } from "@prisma/client";
 
 import { prisma } from "../lib/prisma";
 
-async function createUser(data: {
-  email: string;
-  password?: string;
-  role: Role;
-}) {
+async function createUser(payload: Prisma.UserCreateInput) {
   const user = await prisma.user.create({
     data: {
-      email: data.email,
-      password: data.password,
-      role: data.role,
+      email: payload.email,
+      password: payload.password,
+      role: payload.role,
     },
     select: {
       id: true,
       email: true,
+      password: true,
       role: true,
       isVerified: true,
-      isDeleted: true,
       createdAt: true,
       updatedAt: true,
     },
@@ -27,4 +23,120 @@ async function createUser(data: {
   return { user };
 }
 
-export { createUser };
+async function getUserById(query: { id: string; role?: Role }) {
+  const user = await prisma.user.findFirst({
+    where: {
+      id: query.id,
+      role: query.role,
+      isDeleted: false,
+    },
+    select: {
+      id: true,
+      email: true,
+      password: true,
+      role: true,
+      isVerified: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  return { user };
+}
+
+async function getUserByEmail(query: { email: string; role?: Role }) {
+  const user = await prisma.user.findFirst({
+    where: {
+      email: query.email,
+      role: query.role,
+      isDeleted: false,
+    },
+    select: {
+      id: true,
+      email: true,
+      password: true,
+      role: true,
+      isVerified: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  return { user };
+}
+
+async function updateUserById(
+  query: { id: string; role?: Role },
+  payload: Prisma.UserUpdateInput,
+) {
+  const user = await prisma.user.update({
+    where: {
+      id: query.id,
+      role: query.role,
+      isDeleted: false,
+    },
+    data: payload,
+    select: {
+      id: true,
+      email: true,
+      password: true,
+      role: true,
+      isVerified: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  return { user };
+}
+
+async function updateUserByEmail(
+  query: { email: string; role?: Role },
+  payload: Prisma.UserUpdateInput,
+) {
+  const user = await prisma.user.update({
+    where: {
+      email: query.email,
+      role: query.role,
+      isDeleted: false,
+    },
+    data: payload,
+    select: {
+      id: true,
+      email: true,
+      password: true,
+      role: true,
+      isVerified: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  return { user };
+}
+
+async function deleteUserById(query: { id: string; role?: Role }) {
+  const { user } = await updateUserById(query, {
+    isDeleted: true,
+  });
+
+  return { user };
+}
+
+async function deleteUserByEmail(query: { email: string; role?: Role }) {
+  const { user } = await updateUserByEmail(query, {
+    isDeleted: true,
+  });
+
+  return { user };
+}
+
+export {
+  createUser,
+  getUserById,
+  getUserByEmail,
+  updateUserById,
+  updateUserByEmail,
+  deleteUserById,
+  deleteUserByEmail,
+};
