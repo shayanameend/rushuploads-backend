9e04401e3243b5682b04b80f69b6463443e071d1
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-12-05 12:59:16 +0500

    Add sign-in and OTP verification routes to auth router

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
-import { signUp } from "../controllers/auth";
+import { signIn, signUp, verifyOtp } from "../controllers/auth";
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", signUp);
+authRouter.post("/sign-in", signIn);
+authRouter.post("/verify-otp", verifyOtp);
 
 export { authRouter };
