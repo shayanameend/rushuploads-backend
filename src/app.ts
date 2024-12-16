c82336f0114e06fec85a1a989f25c93e55b40614
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-12-16 12:56:52 +0500

    Enable CORS for express app and socket.io server to allow requests from any origin

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,11 @@ import { userRouter } from "./routers/user";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,11 @@ import { Server as Socket } from "socket.io";
 
 import { server } from "./server";
 
-const io = new Socket(server);
+const io = new Socket(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 io.on("connection", () => {
   console.log("Connected!");
