33b9e1c5668b72b5170d17d6a7cb639320a04b89
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-01-09 09:07:17 +0500

    Update FREE tier constraints: remove limits on send size and storage, extend max expiry to 365 days

--- src/constants/tiers.ts
+++ src/constants/tiers.ts
@@ -3,11 +3,17 @@ import { Tier } from "@prisma/client";
 import { env } from "../lib/env";
 
 const TierConstraints = {
+  // FREE: {
+  //   maxSendSize: 2.5 * 1024 * 1024 * 1024, // 2.5 GB in bytes
+  //   maxStorage: 25 * 1024 * 1024 * 1024, // 25 GB in bytes
+  //   minExpiry: 3 * 24 * 60 * 60 * 1000, // 3 days in milliseconds
+  //   maxExpiry: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+  // },
   FREE: {
-    maxSendSize: 2.5 * 1024 * 1024 * 1024, // 2.5 GB in bytes
-    maxStorage: 25 * 1024 * 1024 * 1024, // 25 GB in bytes
+    maxSendSize: -1, // No limit
+    maxStorage: -1, // No limit
     minExpiry: 3 * 24 * 60 * 60 * 1000, // 3 days in milliseconds
-    maxExpiry: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+    maxExpiry: 365 * 24 * 60 * 60 * 1000, // 365 days in milliseconds
   },
   PRO: {
     maxSendSize: 250 * 1024 * 1024 * 1024, // 250 GB in bytes
